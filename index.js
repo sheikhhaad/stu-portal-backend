cdd3d1623f5dc375e3848bdfbf2f716b371dcb0e
sheikhhaad <sheikhhaad6@gmail.com>
2026-02-17 16:33:51 +0500

    .

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
-app.use(cors('*'))
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
 
 
 app.get("/", (req, res) => {
