743e4116f6f44e5c88206ec7140f2a6f363c3aa9
sheikhhaad <sheikhhaad6@gmail.com>
2026-02-17 17:35:18 +0500

    .

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000", // ya "*" sab allow karne ke liye (dev me)
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
 
 
 app.get("/", (req, res) => {
